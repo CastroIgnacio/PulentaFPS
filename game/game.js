9acd9cdb031d02ae23a0041db35174e44fd42977
Ignacio <ignacio@venzee.com>
2015-05-29 22:24:37 -0300

    add fullscreen options

--- game/game.js
+++ game/game.js
@@ -5,17 +5,27 @@ var engine,
     canvas,
     scene;
 
-
 /**
  * Load the game on content load
  */
-document.addEventListener("DOMContentLoaded", onGameLoad);
+document.addEventListener('DOMContentLoaded', onGameLoad);
 
 function onGameLoad() {
     canvas = document.querySelector('#gameCanvas');
     engine = new BABYLON.Engine(canvas, true);
+
+    // Set as fullscreen and lock pointer on click
+    canvas.addEventListener('click', function(){
+        //only apply fullscreen and lockpointer if is not yet
+        if(!engine.isFullscreen) {
+            engine.switchFullscreen(true);
+        }
+    });
+
+    //Scene create
     scene = createScene();
 
+    //Init render loop
     engine.runRenderLoop(function () {
         scene.render();
     });
